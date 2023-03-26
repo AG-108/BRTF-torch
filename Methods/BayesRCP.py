ce3835b5f01e72033e41a5285fbb427b29561346
AG-108 <57614056+AG-108@users.noreply.github.com>
2023-03-26 13:22:43 +0800

    Add files via upload

--- Methods/BayesRCP.py
+++ Methods/BayesRCP.py
@@ -1,8 +1,16 @@
-from config import *
+import tltorch
+from .config import *
 import torch
+import numpy as np
+import tensorly as tl
 
 def ten2mat(tensor, mode):
-    return tensor.moveaxis(mode, 0).reshape((tensor.shape[mode], -1))
+    return torch.tensor(
+        np.reshape(
+            np.moveaxis(tensor.numpy(), mode, 0),
+            (tensor.shape[mode], -1),
+            order='F')
+    )
 
 def BayesRCP(Y, init = INIT_ML, maxRank = None, dimRed = 1, initVar = 1, updateHyper = UPDATEHYPER_ON, maxIters = 100, tol = 1e-5, predVar = PREDVAR_DOES_NOT_COMPUTE, verbose = VERBOSE_TEXT):
     '''
@@ -28,9 +36,9 @@ def BayesRCP(Y, init = INIT_ML, maxRank = None, dimRed = 1, initVar = 1, updateH
 
     # Initialization
 
-    dimY = Y.size();
-    N = dimY.size(dim=0);
-    nObs = prod(dimY)
+    dimY = torch.tensor(Y.size())
+    N = dimY.size(dim=0)
+    nObs = dimY.prod()
     LB = 0
 
     a_gamma0 = 1e-6
@@ -41,12 +49,12 @@ def BayesRCP(Y, init = INIT_ML, maxRank = None, dimRed = 1, initVar = 1, updateH
     b_alpha0 = 1e-6
     eps = 1e-15
 
-    gammas = (a_gamma0+eps)/(b_gamma0+eps)*torch.ones(R,1);
+    gammas = (a_gamma0+eps)/(b_gamma0+eps)*torch.ones(maxRank,1);
     beta = (a_beta0+eps)/(b_beta0+eps);
-    alphas = 1/initVar*torch.ones(dimY)*((a_alpha0+eps)/(b_alpha0+eps));
+    alphas = 1/initVar*torch.ones(tuple(dimY))*((a_alpha0+eps)/(b_alpha0+eps));
 
-    E = 1/alphas.sqrt()*torch.randn(dimY);
-    Sigma_E = 1/alphas*torch.ones(dimY);
+    E = 1/alphas.sqrt()*torch.randn(tuple(dimY));
+    Sigma_E = 1/alphas*torch.ones(tuple(dimY));
 
     if not isinstance(init,int):
         pass
@@ -55,7 +63,7 @@ def BayesRCP(Y, init = INIT_ML, maxRank = None, dimRed = 1, initVar = 1, updateH
         #     error('OPTS.init does not have %d cells',N);
         # end
         # for n = 1:N;
-        #     if ~isequal(size(Z{n}),[size(Y,n) R])
+        #     if ~isequal(size(Z{n}),[size(Y,n) maxRank])
         #         error('OPTS.init{%d} is the wrong size',n);
         #     end
         # end
@@ -63,27 +71,56 @@ def BayesRCP(Y, init = INIT_ML, maxRank = None, dimRed = 1, initVar = 1, updateH
         if init == INIT_ML:
             Z = []
             ZSigma = []
+            for n in range(N):
+                ZSigma.append((1/gammas).diag())
+                [U, S, Vh] = torch.linalg.svd(ten2mat(Y, n))
+                S = S.diag()
+                V = Vh.mH
+                dvar = (Y.square().sum()/nObs).sqrt()
+                if maxRank <= U.size(dim=1):
+                    Z.append(U[:,0:maxRank].matmul(S[0:maxRank,0:maxRank].sqrt()))
+                else:
+                    Z.append(torch.cat(U.matmul(S.sqrt()), torch.randn(dimY[n], maxRank-U.size(dim=1))*dvar))
+        elif init == INIT_RAND:
+            Z = []
+            ZSigma = []
             for n in range(n):
-                ZSigma[n] = (1/gammas).diag()
+                ZSigma.append((1/gammas).diag())
+                dvar = (Y.square().sum() / nObs).sqrt()
+                Z.append(torch.randn(dimY[n], maxRank) * dvar)
 
-X = torch.tensor([[[1, 2, 3, 4], [3, 4, 5, 6]],
-              [[5, 6, 7, 8], [7, 8, 9, 10]],
-              [[9, 10, 11, 12], [11, 12, 13, 14]]])
+    # create figure -- remain to be solved
 
-print('tensor size:')
-print(X.shape)
-print()
-print('切片矩阵：X[:, :, 1] =')
-print(X[:, :, 0])
-print()
-print('切片矩阵：X[:, :, 2] =')
-print(X[:, :, 1])
-print()
-print('切片矩阵：X[:, :, 3] =')
-print(X[:, :, 2])
-print()
-print('切片矩阵：X[:, :, 4] =')
-print(X[:, :, 3])
-print()
-print('模态1展开矩阵 (mode-1 tensor unfolding):')
-print(ten2mat(X, 0))
+    # model learning
+    EZZT = []
+    for n in range(N):
+        EZZT.append(Z[n].mH.matmul(Z[n]) + dimY[n]*ZSigma[n])
+
+    for it in range(maxIters):
+        Aw = gammas.diag()
+        for n in range(N):
+            ENZZT = torch.ones(maxRank,maxRank)
+            for m in range(N):
+                if m != n:
+                    ENZZT = ENZZT * EZZT[m]
+            FslashY = tl.tenalg.khatri_rao(Z[0:n]+Z[n+1:N], reverse=True).mH.matmul(ten2mat(Y-E,n).mH)
+            ZSigma[n] = 1/(beta * ENZZT + Aw)
+            Z[n] = (beta * ZSigma[n]).matmul(FslashY)
+            EZZT[n] = Z[n].mH.matmul(Z[n]) + dimY[n] * ZSigma[n]
+
+    print(len(Z))
+    X = tltorch.CPTensor(torch.ones(Z[0].size(dim=1)),Z).to_tensor()
+
+    a_gammaN = (0.5*dimY.sum() + a_gamma0) * torch.ones(maxRank,1)
+    b_gammaN = 0
+    for n in range(N):
+        b_gammaN = b_gammaN + (Z[n].mH.matmul(Z[n])).diag() + dimY[n] * ZSigma[n].diag
+    b_gammaN = b_gamma0 + 0.5 * b_gammaN
+    gammas = a_gammaN/b_gammaN
+
+    EX2 = torch.ones(maxRank,maxRank)
+    for n in range(N):
+        EX2 = EX2 * EZZT[n]
+    EX2 = EX2.sum(dim=0)
+    return None
+
