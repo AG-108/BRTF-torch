565d0ffd37f0889b3cabfeb00667c4440523484b
AG-108 <57614056+AG-108@users.noreply.github.com>
2023-03-28 15:54:05 +0800

    Add files via upload

--- DemoBayesRCP.py
+++ DemoBayesRCP.py
@@ -8,8 +8,9 @@ import numpy as np
 from scipy import signal
 import tltorch as tl
 import matplotlib
+from Methods.BayesRCP import *
 
-Dimension = torch.tensor([40,40,40])
+Dimension = torch.tensor([30,30,30])
 R = 5
 DataType = 2
 
@@ -24,20 +25,17 @@ elif DataType == 2:
     for i in range(Dimension.size(dim=0)):
         # Caution: float error
         temp = torch.arange(0,2*(i+1)*torch.pi*(Dimension[i]-0.5)/(Dimension[i]-1),2*(i+1)*torch.pi/(Dimension[i]-1)).unsqueeze(1)
-        part1 = torch.cat([temp.sin(), temp.cos(), torch.tensor(signal.square(2*temp+1e-5))],1)
-        mean = part1.sum(dim=0)/Dimension[i]
-        std = part1.std(dim=0)
-        part1 = (part1 - mean)/std
+        part1 = torch.cat([temp.sin(), temp.cos(), torch.tensor(signal.square(2*temp+1e-5),dtype=torch.float32)],1)
         part2 = torch.normal(
             torch.zeros([Dimension[i],R-3]),
             torch.ones([Dimension[i],R-3]),
         )
         Z.append(torch.cat([part1,part2],1))
 
-X = tl.CPTensor(torch.ones(R),Z).to_tensor()
+X = tltorch.CPTensor(torch.ones(R),Z).to_tensor()
 
 SNR = 20
-sigma2 = torch.var(X)*(1/(10**(SNR/10)))
+sigma2 = X.var()*(1/(10**(SNR/10)))
 GN = sigma2.sqrt()*torch.randn(tuple(Dimension))
 
 SparseRatio = 0.05
@@ -48,21 +46,18 @@ S.view(-1)[Somega] = X.max()*(2*torch.rand(Somega.size(dim=0),1)-1).squeeze()
 
 Y = X + S + GN
 
-SNR = 10*X.var().log10()/((Y-X).var())
-print(SNR)
+SNR = 10*((X.var()/(Y-X).var()).log10())
 
 print('------Bayesian Robust CP Factorization-------------')
 
 start = time.time()
-# model = BayesRCP_TC()
+model = BayesRCP(Y, maxRank = max(list(Dimension)+[2*R]),verbose = VERBOSE_IMAGE_PLOT)
 t_total = time.time() - start
 
-# X_BRCP = tltorch.CPTensor(model.Z).to_tensor()
-# Err = X_BRCP - X
-# rrse = (Err.square().sum())/(X.square().sum()).sqrt()
-# rmse = Err.square().mean().sqrt()
-rrse = 0.0212369
-rmse = 0.0127324
+X_BRCP = tltorch.CPTensor(model.Z).to_tensor()
+Err = X_BRCP - X
+rrse = (Err.square().sum())/(X.square().sum()).sqrt()
+rmse = Err.square().mean().sqrt()
 
 print('''
 ------------------------BRCPF Result---------------------------------------------
@@ -87,8 +82,6 @@ for i in errors:
 
 # rrse = (Err.square().sum())/(X.square().sum()).sqrt()
 # rmse = Err.square().mean().sqrt()
-rrse = 0.0212369
-rmse = 0.0127324
 
 print('''-------------CP-ALS------------------------------------------
 RRSE = {:.7f}, RMSE = {:.7f},
